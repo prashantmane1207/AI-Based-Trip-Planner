19ee570aa287ce8277f3ba8568a1a3c0f9c94158
prashantmane1207 <prashantmane1207@gmail.com>
2026-05-27 12:10:27 +0530

    Update api.js

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Automatically picks URL based on environment (Vite uses import.meta.env)
+
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export const saveTrip = async (tripData) => {
